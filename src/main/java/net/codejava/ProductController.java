8135bd178f81ea42bbd5bb69d51124f3571a15f3
rajaramadoss <raja90r@outlook.com>
2022-01-03 07:49:42 +0530

    crud operation working and used postman tool and mysql db also

--- src/main/java/net/codejava/ProductController.java
+++ src/main/java/net/codejava/ProductController.java
@@ -37,7 +37,7 @@ public class ProductController {
 		}
 	}
 	
-	@PostMapping("/products")
+	@PostMapping("/products") // Insert Functions
 	public void add(@RequestBody Product product) {
 		service.save(product);
 	}
